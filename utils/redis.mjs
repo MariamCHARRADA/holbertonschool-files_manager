351cd01aac71146d229ea2bef14e08bed287ce77
MariamCHARRADA <charradamariam@yahoo.com>
2024-08-01 17:31:30 +0100

    not a func?

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,10 +1,14 @@
 import { createClient } from "redis";
+import { promisify } from "util";
 
 class RedisClient {
   constructor() {
     this.client = createClient();
     this.isConnected = false;
 
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setexAsync = promisify(this.client.setex).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
 
     this.client.on("error", (err) => {
       console.log(err);
@@ -21,18 +25,17 @@ class RedisClient {
   }
 
   async get(key) {
-    return this.client.get("key", function (err, reply) {
-      console.log(reply);
-    });
+    return await this.getAsync(key);
   }
 
   async del(key) {
-    return this.client.del(key);
+    return await this.delAsync(key);
   }
 
   async set(key, value, duration) {
-    return this.client.set(key, value, duration);
+    return await this.setexAsync(key, value, duration);
   }
 }
 
+const redisClient = new RedisClient();
 export default RedisClient;
