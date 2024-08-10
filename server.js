cdb5b5b0aa1e0e8a994b4406ce905f282473f636
MariamCHARRADA <charradamariam@yahoo.com>
2024-08-10 20:17:21 +0100

    fix server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-import express from "express";
-import router from "./routes/index.mjs";
+const express = require("express");
+const router = require("./routes/index.js");
 
 const app = express();
 
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-
-const port = process.env.PORT || 5000;
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
