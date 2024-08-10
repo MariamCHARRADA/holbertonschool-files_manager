abcfcc5497e665f3b8e48e859a5abe9dbc9b0b44
MariamCHARRADA <charradamariam@yahoo.com>
2024-08-10 13:58:15 +0100

    fix server

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 import express from "express";
 import router from "./routes/index.mjs";
-import bodyParser from "body-parser";
-
-const express = require("express");
 
 const app = express();
-app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+const port = process.env.PORT || 5000;
 
 app.use("/", router);
 
